f2715de951f3037d0adff8b5a1e0d60263680312
gdrclm <112739588+gdrclm@users.noreply.github.com>
2024-04-18 13:37:47 +0300

    Update validation.js

--- scripts/validation.js
+++ scripts/validation.js
@@ -4,33 +4,7 @@ window.addEventListener("click", function () {
     input.setAttribute("required", "");
   }
 });
-// document.addEventListener("DOMContentLoaded", function () {
-//   // Удалить атрибут required у всех полей ввода
-//   var inputs = document.querySelectorAll("input[required]");
-//   inputs.forEach(function (input) {
-//     input.removeAttribute("required");
-//   });
 
-//   // Добавить обработчик события клика на форму
-//   document.getElementById("myForm").addEventListener("click", function (event) {
-//     var target = event.target;
-//     if (target.tagName === "INPUT" && target.type === "text") {
-//       // Проверить, был ли клик по полю ввода
-//       // Если да, добавить атрибут required к этому полю
-//       target.setAttribute("required", "");
-//     }
-//   });
-
-//   // Добавить обработчик события отправки формы
-//   document
-//     .getElementById("myForm")
-//     .addEventListener("submit", function (event) {
-//       var inputs = document.querySelectorAll("input[type='text']");
-//       inputs.forEach(function (input) {
-//         input.setAttribute("required", "");
-//       });
-//     });
-// });
 window.addEventListener("DOMContentLoaded", function () {
   // Удалить атрибут required у всех полей ввода
   var inputs = document.querySelectorAll("input[required]");
